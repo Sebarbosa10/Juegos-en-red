93319fecabd8be98e283bc815b165aa3f42ec6c4
Nicolas Saldeno <skyladdamebusiness@gmail.com>
2025-09-16 17:20:46 -0300

    SARCOPHAGUS PUZZLE DONE

--- Assets/Script/Egypt/SarcophagusPuzzle.cs
+++ Assets/Script/Egypt/SarcophagusPuzzle.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using System.Collections;
 using System.Collections.Generic;
 
 public class SarcophagusPuzzle : MonoBehaviour
 {
     [Header("Config")]
-    public SarcophagusLid[] lids;  
-    public int[] correctOrder;          
-    [Tooltip("QUE ES ESTO DIOS, ESTOY CHANCHEANDO")]
+    public SarcophagusLid[] lids;
+    public int[] correctOrder;
+    [Tooltip("ODIO TODO")]
     public bool animatedReset = true;
 
-    // Estado
+    [Header("Events")]
+    public UnityEvent onSolved;   
+
     private readonly List<int> attemptOrder = new List<int>();
     private readonly HashSet<int> reserved = new HashSet<int>();
     private bool solved = false;
@@ -23,19 +26,16 @@ public class SarcophagusPuzzle : MonoBehaviour
         if (lids != null && correctOrder != null)
         {
             foreach (var i in correctOrder)
-            {
                 if (i < 0 || i >= lids.Length)
                     Debug.LogWarning($"Índice {i} fuera de rango en 'correctOrder'", this);
-            }
         }
     }
-
 
     public bool TryReserveIndex(int index)
     {
         if (IsBusy) return false;
-        if (reserved.Contains(index)) return false;                         
-        if (attemptOrder.Count >= correctOrder.Length) return false;        
+        if (reserved.Contains(index)) return false;
+        if (attemptOrder.Count >= correctOrder.Length) return false;
         reserved.Add(index);
         return true;
     }
@@ -45,15 +45,13 @@ public class SarcophagusPuzzle : MonoBehaviour
         if (IsBusy) return;
 
         attemptOrder.Add(index);
+
         if (attemptOrder.Count >= correctOrder.Length)
-        {
             StartCoroutine(EvaluateAfterAnimations());
-        }
     }
 
     private IEnumerator EvaluateAfterAnimations()
     {
-
         bool anyAnimating;
         do
         {
@@ -74,20 +72,18 @@ public class SarcophagusPuzzle : MonoBehaviour
         if (ok)
         {
             for (int i = 0; i < correctOrder.Length; i++)
-            {
                 if (attemptOrder[i] != correctOrder[i]) { ok = false; break; }
-            }
         }
 
         if (ok)
         {
             solved = true;
             Debug.Log("VAMOOOOOOOO");
-     
+            onSolved?.Invoke();            
         }
         else
         {
-            Debug.Log("MALISIMO TODO. TU LOGICA Y LA MATERIA");
+            Debug.Log("ADSASDADSADSADSASDADSADSt");
             StartCoroutine(ResetAttemptRoutine());
         }
     }
@@ -96,7 +92,6 @@ public class SarcophagusPuzzle : MonoBehaviour
     {
         resetting = true;
 
-
         foreach (var lid in lids)
         {
             if (lid == null) continue;
@@ -104,7 +99,6 @@ public class SarcophagusPuzzle : MonoBehaviour
             if (animatedReset) lid.AnimateBackToInitial();
             else lid.SnapToInitial();
         }
-
 
         if (animatedReset)
         {
@@ -127,8 +121,7 @@ public class SarcophagusPuzzle : MonoBehaviour
 
         attemptOrder.Clear();
         reserved.Clear();
-
         resetting = false;
-        Debug.Log("NOOOOOOOOOOO");
+        Debug.Log("↩️NOOOOOOOOOO");
     }
 }
---
+++ Assets/Script/GATES/VerticalGate.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class VerticalGate : MonoBehaviour
+{
+    [Header("ANIMATED INTERACTIBLE")]
+    public AnimatedInteractable mover; 
+
+    [Tooltip("aaaaaaaaaaaaaaaaaaaaaaaaaaa")]
+    public bool forceOpen = true;
+
+    public void Open()
+    {
+        if (mover == null) return;
+        if (forceOpen)
+        {
+ 
+            AnimatedManager.Instance.ResetAnimatedToInitial(mover, markClosed: false); 
+            AnimatedManager.Instance.HandleInteraction(mover); 
+            mover.isActive = true;
+        }
+        else
+        {
+            mover.Interact(); 
+        }
+    }
+
+    public void Close()
+    {
+        if (mover == null) return;
+        AnimatedManager.Instance.CancelAndSnapToInitial(mover, markClosed: true);
+    }
+}
