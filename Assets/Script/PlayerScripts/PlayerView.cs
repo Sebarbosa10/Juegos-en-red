f1e35528717cfab0857e17fec263f25c4840857d
Nicolas Saldeno <skyladdamebusiness@gmail.com>
2025-09-16 17:41:56 -0300

    REVISITING SPAWN AND CONNECTION

--- Assets/Script/PlayerScripts/PlayerView.cs
+++ Assets/Script/PlayerScripts/PlayerView.cs
@@ -5,7 +5,6 @@ public class PlayerView : MonoBehaviour
     public Camera playerCamera;
     public GameObject handIcon;
 
-
     public void Move(Vector3 movement)
     {
         transform.Translate(movement);
@@ -18,11 +17,25 @@ public class PlayerView : MonoBehaviour
 
     public void RotateCamera(float xRotation)
     {
-        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        if (playerCamera != null)
+            playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
 
     public void ShowHandIcon(bool show)
     {
-        handIcon.SetActive(show);
+        if (handIcon != null) handIcon.SetActive(show);
+    }
+
+
+    public void SetLocalCameraActive(bool isLocal)
+    {
+        if (playerCamera != null) playerCamera.enabled = isLocal;
+
+
+        var al = playerCamera != null ? playerCamera.GetComponent<AudioListener>() : null;
+        if (al != null) al.enabled = isLocal;
+
+
+        if (!isLocal && handIcon != null) handIcon.SetActive(false);
     }
 }
