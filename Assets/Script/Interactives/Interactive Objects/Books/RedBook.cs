dabcb3787fb8e805076d456538e2786ec6348c45
Nicolas Saldeno <skyladdamebusiness@gmail.com>
2025-09-18 13:54:13 -0300

    trying note again ◀️

--- Assets/Script/Interactives/Interactive Objects/Books/RedBook.cs
+++ Assets/Script/Interactives/Interactive Objects/Books/RedBook.cs
@@ -1,14 +1,53 @@
+using TMPro;
 using UnityEngine;
 public class RedBook : MonoBehaviour, IInteractive
 {
-    [SerializeField] private PlayerController cameraController;
+    [Header("UI del prefab")]
+    [SerializeField] private GameObject notePanel;  
+
+    [Header("CONTENT")]
+    private bool _isOpen = false;
+    private CursorLockMode _prevLockMode;
+    private bool _prevCursorVisible;
+
+    private void Awake()
+    {
+        if (notePanel != null) notePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (_isOpen && Input.GetKeyDown(KeyCode.Escape))
+            Close();
+    }
 
     public void Interact()
     {
-        if (!UIManager.Instance.IsBookOpen)
-            UIManager.Instance.OpenBook(cameraController);   
-        else
-            UIManager.Instance.CloseBook(cameraController);  
+        if (_isOpen) Close();
+        else Open();
+    }
+
+    public void Open()
+    {
+        if (notePanel == null) return;
+
+        notePanel.SetActive(true);
+        _isOpen = true;
+
+        _prevLockMode = Cursor.lockState;
+        _prevCursorVisible = Cursor.visible;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Close()
+    {
+        if (notePanel == null) return;
+
+        notePanel.SetActive(false);
+        _isOpen = false;
+        Cursor.visible = _prevCursorVisible;
+        Cursor.lockState = _prevLockMode;
     }
 }
 
