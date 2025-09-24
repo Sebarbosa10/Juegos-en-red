b5914a738d87219698e3c84b92071689150f1831
SatoruuGoyoo <mentabullrichgregorio@gmail.com>
2025-09-24 18:23:15 -0300

    FinalCommit

--- Assets/Script/PUN & Menu/MainMenuLauncher.cs
+++ Assets/Script/PUN & Menu/MainMenuLauncher.cs
@@ -11,8 +11,8 @@ public class MainMenuLauncher : MonoBehaviourPunCallbacks
     public Button connectButton;
 
     [Header("Scenes")]
-    [SerializeField] private string lobbySceneName = "Test"; // Lobby
-    [SerializeField] private string roomName = "Test";  //EgyptLobby
+    [SerializeField] private string lobbySceneName = "Lobby"; // Lobby
+    [SerializeField] private string roomName = "Lobby";  //EgyptLobby
     [SerializeField] private byte maxPlayers = 4;
 
     private string nickname;
