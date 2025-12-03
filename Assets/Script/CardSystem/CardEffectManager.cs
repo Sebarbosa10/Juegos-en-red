faab3d15a0b2128752fa48d2cc29ceab1f585860
Sebarbosa10 <sebastianbarbosa10@gmail.com>
2025-12-03 19:56:11 -0300

    chanchadas

--- Assets/Script/CardSystem/CardEffectManager.cs
+++ Assets/Script/CardSystem/CardEffectManager.cs
@@ -15,10 +15,11 @@ public class CardEffectManager : MonoBehaviour
     private float _baseMouseY;
     private bool _baseIsSlippery;
 
-   
-    private Coroutine _randomSensRoutine;
-    private Coroutine _lightingStopRoutine;
-    private Coroutine _heavyWeightRoutine;
+    
+    private Coroutine _activeEffectRoutine;
+
+    
+    private CardEffectType? _currentEffect = null;
 
     private void Awake()
     {
@@ -26,7 +27,6 @@ public class CardEffectManager : MonoBehaviour
         _model = GetComponent<PlayerModel>();
         _pv = GetComponent<PhotonView>();
 
-        
         _baseSpeed = _model.Speed;
         _baseSprintSpeed = _model.SprintSpeed;
         _baseMouseX = _model.MouseSensivityX;
@@ -37,53 +37,62 @@ public class CardEffectManager : MonoBehaviour
     public void ActivateEffects(CardData card)
     {
         if (_pv != null && !_pv.IsMine) return;
+        if (card == null) return;
+
+        Debug.Log($"[CardEffectManager] Activando efecto: {card.cardEffectType}");
+
+        ResetAllEffects();
+
+        _currentEffect = card.cardEffectType;
 
         switch (card.cardEffectType)
         {
             case CardEffectType.SlipperyFeet:
-                StartCoroutine(DoSlipperyFeet());
+                _activeEffectRoutine = StartCoroutine(DoSlipperyFeet());
                 break;
 
             case CardEffectType.RandomSensitivity:
-                if (_randomSensRoutine != null)
-                    StopCoroutine(_randomSensRoutine);
-                _randomSensRoutine = StartCoroutine(DoRandomSensivity());
+                _activeEffectRoutine = StartCoroutine(DoRandomSensitivity());
                 break;
 
             case CardEffectType.LightingStop:
-                if (_lightingStopRoutine != null)
-                    StopCoroutine(_lightingStopRoutine);
-                _lightingStopRoutine = StartCoroutine(DoLightingStop());
+                _activeEffectRoutine = StartCoroutine(DoLightingStop());
                 break;
 
             case CardEffectType.HeavyWeight:
-                if (_heavyWeightRoutine != null)
-                    StopCoroutine(_heavyWeightRoutine);
-                _heavyWeightRoutine = StartCoroutine(DoHeavyWeight());
+                _activeEffectRoutine = StartCoroutine(DoHeavyWeight());
                 break;
         }
     }
 
     private IEnumerator DoSlipperyFeet()
     {
-        
-        _model.IsSlippery = true; 
+        Debug.Log("[CardEffectManager] SlipperyFeet activado");
+        _model.IsSlippery = true;
+
+       
+        ShowEffectUI();
+
         yield break;
     }
 
-    private IEnumerator DoRandomSensivity()
+    private IEnumerator DoRandomSensitivity()
     {
-        
+        Debug.Log("[CardEffectManager] RandomSensitivity activado");
 
         while (true)
         {
+            
             _model.MouseSensivityX = Random.Range(0.5f, 20f);
             _model.MouseSensivityY = Random.Range(0.5f, 20f);
+            ShowEffectUI(); 
 
             yield return new WaitForSeconds(25f);
 
+            
             _model.MouseSensivityX = _baseMouseX;
             _model.MouseSensivityY = _baseMouseY;
+            HideEffectUI(); 
 
             yield return new WaitForSeconds(5f);
         }
@@ -91,65 +100,102 @@ public class CardEffectManager : MonoBehaviour
 
     private IEnumerator DoLightingStop()
     {
-        
+        Debug.Log("[CardEffectManager] LightingStop activado");
 
         while (true)
         {
+            
+            HideEffectUI();
             yield return new WaitForSeconds(12.5f);
+
+            
             _controller.SetCanMove(false);
+            ShowEffectUI();
+
             yield return new WaitForSeconds(5f);
+
+            
             _controller.SetCanMove(true);
         }
     }
 
     private IEnumerator DoHeavyWeight()
     {
-        
+        Debug.Log("[CardEffectManager] HeavyWeight activado");
 
         while (true)
         {
-            _model.SetMovementSpeed(_baseSpeed / 2f, _baseSpeed / 2f);
+            
+            _model.SetMovementSpeed(_baseSpeed / 2f, _baseSprintSpeed / 2f);
+            ShowEffectUI();
+
             yield return new WaitForSeconds(20f);
 
+            
             _model.SetMovementSpeed(_baseSpeed, _baseSprintSpeed);
+            HideEffectUI();
+
             yield return new WaitForSeconds(10f);
         }
     }
 
-  
+    private void ShowEffectUI()
+    {
+        if (CardEffectUI.Instance != null)
+        {
+            CardEffectUI.Instance.ShowEffectActive();
+        }
+    }
+
+    private void HideEffectUI()
+    {
+        if (CardEffectUI.Instance != null)
+        {
+            CardEffectUI.Instance.HideEffectActive();
+        }
+    }
+
     public void ResetAllEffects()
     {
-        
-        if (_randomSensRoutine != null)
+        Debug.Log("[CardEffectManager] Reseteando todos los efectos...");
+
+        if (_activeEffectRoutine != null)
         {
-            StopCoroutine(_randomSensRoutine);
-            _randomSensRoutine = null;
+            StopCoroutine(_activeEffectRoutine);
+            _activeEffectRoutine = null;
         }
 
-        if (_lightingStopRoutine != null)
-        {
-            StopCoroutine(_lightingStopRoutine);
-            _lightingStopRoutine = null;
-        }
-
-        if (_heavyWeightRoutine != null)
-        {
-            StopCoroutine(_heavyWeightRoutine);
-            _heavyWeightRoutine = null;
-        }
-
-      
         _model.IsSlippery = _baseIsSlippery;
         _model.SetMovementSpeed(_baseSpeed, _baseSprintSpeed);
         _model.MouseSensivityX = _baseMouseX;
         _model.MouseSensivityY = _baseMouseY;
 
-       
         if (_controller != null)
         {
             _controller.SetCanMove(true);
         }
 
         
+        HideEffectUI();
+
+        _currentEffect = null;
+    }
+
+    public CardEffectType? GetCurrentEffect()
+    {
+        return _currentEffect;
+    }
+
+    public bool HasActiveEffect()
+    {
+        return _currentEffect.HasValue;
+    }
+
+    private void OnDestroy()
+    {
+        if (_activeEffectRoutine != null)
+        {
+            StopCoroutine(_activeEffectRoutine);
+        }
     }
 }
